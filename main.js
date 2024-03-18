5109c57499631b72fd03964a985a00406aee94fa
ma278hdi <monday22220@gmail.com>
2024-03-19 02:14:53 +0300

    a

--- main.js
+++ main.js
@@ -1,2 +1,8 @@
-var elementList = [];
+var list1 = []
+document.querySelector('.btn').addEventListener('click',()=>{
+    list1.push(document.querySelector('.card-text').innerHTML)
+
+})
+
+alert(list1[0])
 
